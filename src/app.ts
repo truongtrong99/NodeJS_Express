7b49f387828cbb4c2b18a45246001096b87f1a73
truongtrong99 <truongtrong400@gmail.com>
2025-08-01 14:33:13 +0700

    #30 - Add dotenv support and update .gitignore; configure environment variables

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,14 @@
 // const express = require('express');
 import express from 'express';
-
+import 'dotenv/config'; // Automatically loads environment variables from .env file
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080; // Use PORT from .env or default to 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World nodemon!');
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
+  console.log("check env port: ", process.env.PORT);
 })
