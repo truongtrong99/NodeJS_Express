15a54c9cfdd0f33d9ccf56fb782dd528bbdf9c93
truongtrong99 <truongtrong400@gmail.com>
2025-08-01 15:28:06 +0700

    #32 - Add EJS view engine support and create home view template

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,12 @@ import express from 'express';
 import 'dotenv/config'; // Automatically loads environment variables from .env file
 const app = express();
 const port = process.env.PORT || 8080; // Use PORT from .env or default to 3000
+//config view engine
+app.set('view engine', 'ejs');
+app.set('views',  __dirname + '/views');
 
 app.get('/', (req, res) => {
-  res.send('Hello World nodemon!');
+  res.render('home')
 });
 
 app.listen(port, () => {
